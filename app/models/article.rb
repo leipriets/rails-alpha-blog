46bbf1d22d925bb6529d1661095e6f089201cd71
leipriets <leipriets@gmail.com>
2021-02-24 22:23:11 +0800

    Add article category many-to-many association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
-    # Many to One Assocation to User
+    # Many to One Assocated with User
     belongs_to :user
+    # Many to Many Assocated with article_categories table and category
+    has_many :article_categories
+    has_many :categories, through: :article_categories
 
     validates :title, presence: true, length: { minimum: 6, maximum: 100 }
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
+
     validates :name, presence: true, length: { minimum: 3, maximum: 25 }
     validates_uniqueness_of :name
+ 
+    # Many to Many Assocated with article_categories table and article
+    has_many :article_categories
+    has_many :articles, through: :article_categories
 end
